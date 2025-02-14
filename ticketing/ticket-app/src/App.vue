b6cbb8294fbfd594668f4a29336ad1da82a25898
ArnutDev <arnutbuadonpai@gmail.com>
2025-02-14 14:02:17 +0700

    Update API

--- ticketing/ticket-app/src/App.vue
+++ ticketing/ticket-app/src/App.vue
@@ -1,15 +1,16 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <GetData msg="Welcome to Vue.js!" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// import HelloWorld from './components/HelloWorld.vue'
+import GetData from './components/GetData.vue';
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    GetData
   }
 }
 </script>
---
+++ ticketing/ticket-app/src/components/GetData.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h1>hello yahoooooooo</h1>
+    <p>{{ msg }}</p>
+    <button @click="fetchData">โหลดข้อมูล</button>
+    <h1>{{ data.title }}</h1>
+    <p>{{ data.description }}</p>
+    <p>{{ data.contactInformation }}</p>
+    <p>{{ data.createdTimestamp }}</p>
+    <p>{{ data.updatedTimestamp }}</p>
+    <p>{{ data.status }}</p>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "GetData",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      data: null,
+    };
+  },
+  methods: {
+    async fetchData() {
+      try {
+        const response = await axios.get("http://localhost:8080/api/tickets");
+        this.data = response.data;
+      } catch (error) {
+        console.error("Error something wrong:", error);
+      }
+    },
+  },
+};
+</script>
+<style>
+</style>
